5aa73166c6cac6a4a00e3c5406a9809d74f6aa86
jerry <jerry.splat@gmail.com>
2015-08-26 09:32:29 +0000

    Update method name

--- src/cadet/core.clj
+++ src/cadet/core.clj
@@ -7,20 +7,20 @@
 (defn stay-is-best? [good-option initial-choice]
   (== good-option initial-choice))
 
-(defn run-monte-carlo [number-of-options iterations current-results]
+(defn run-monte-hall [number-of-options iterations current-results]
   (if (== iterations 0)
     current-results
-    (run-monte-carlo number-of-options
-                     (- iterations 1)
-                     (into current-results (if (stay-is-best? (rand-int number-of-options)
-                                                              (rand-int number-of-options))
-                                             ["Stay"]
-                                             ["Change"]
-                                             ))))
+    (run-monte-hall number-of-options
+                    (- iterations 1)
+                    (into current-results (if (stay-is-best? (rand-int number-of-options)
+                                                             (rand-int number-of-options))
+                                            ["Stay"]
+                                            ["Change"]
+                                            ))))
   )
 
 (defn -main
   [& args]
-  (println (frequencies (run-monte-carlo (parse-int (first args))
-                                         (parse-int (nth args 1))
-                                         []))))
+  (println (frequencies (run-monte-hall (parse-int (first args))
+                                        (parse-int (nth args 1))
+                                        []))))
